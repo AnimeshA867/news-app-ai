1a991f7f4f99b4b9a52f532aeafea891ec5e25e4
AnimeshA867 <animeshacharya867+github@gmail.com>
2025-05-07 20:23:14 +0545

    fix: update redirect path in NewCategoryPage to point to edit route

--- app/(auth)/admin/(protected)/categories/new/page.tsx
+++ app/(auth)/admin/(protected)/categories/new/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 export default function NewCategoryPage() {
   // This just redirects to the dynamic [id] route with "new" as the ID
-  redirect("/admin/categories/new");
+  redirect("/admin/categories/new/edit");
 
   // This component won't actually render anything since we're redirecting
   return null;
